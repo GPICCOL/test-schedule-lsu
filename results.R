fbf515e50737b0b024c5ccf1d2f16032229897af
GPICCOL <gpiccoli@cct.lsu.edu>
2018-09-21 15:29:34 -0500

    working resutls

--- results.R
+++ results.R
@@ -12,7 +12,7 @@ scrp_fun <- function(flg) {
   id <- get(paste0("id", flg))
   pwd <- get(paste0("pwd", flg))
   
-  target <- paste0("https://www.cae.lsu.edu/instr_getresults2.asp?selCourse", section, "&testid=", test)
+  target <- paste0("https://www.cae.lsu.edu/instr_getresults2.asp?selCourse=", section, "&testid=", test)
   target2 <- paste0("https://www.cae.lsu.edu/temp/ISDS110200", flg, ".txt")
   
   # Session and login
@@ -21,28 +21,23 @@ scrp_fun <- function(flg) {
   form <- set_values(form, pawsid = id, pwd = pwd)
   submit_form(session = session, form = form)
   
+  logged_in <- submit_form(session = session, form = form)
+  
   # Data extraction
-  jump_to(x = session, url = target)
-  df <- read.delim(target2,header = FALSE)
-  colnames(df) <- c("id", "name", "section", "date", "time", "q_asked", "q_correct", "percentage")
+  df <- logged_in %>% 
+    jump_to(url = target) %>% 
+    html_table(trim = TRUE)
+
+df <- read.delim(target2,header = FALSE)
+colnames(df) <- c("id", "name", "section", "date", "time", "q_asked", "q_correct", "percentage")
   df$section <- flg
-    # read_html() %>% 
-    # html_node("form") %>%
-    # html_node("a")
-    # html_table(trim = TRUE)
-    # 
-    # 
-    # 
-    # html_table(trim = TRUE)
-    # 
-    # tb <- readHTMLTable("temp.html", which = 2)
   
   # Results
   return(df)
 #  write.csv(file = paste0("tests/", "res_s", flg, test_name, tm, ".csv"), df, row.names = FALSE) 
 }
 
-df <- do.call(rbind, lapply(c(1:6), scrp_fun))
+df <- do.call(rbind, lapply(c(1:6,8), scrp_fun))
 df$time <- paste0(df$date, " ", df$time)
 df$date <- as.Date(df$date, format = "%m/%d/%Y")
 df$timestamp <- strptime(df$time, format = "%m/%d/%Y %I:%M:%S %p", tz = "America/Chicago")
